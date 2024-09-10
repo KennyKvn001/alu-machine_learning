738a513c0a677eb9b3253410e6cc22068a2d0e82
KvnKl007 <klakkent99@gmail.com>
2024-09-10 19:27:59 +0200

    document

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,6 +2,9 @@
 
 
 def matrix_shape(matrix):
+    """
+    This function takes a matrix as input and returns its shape.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
