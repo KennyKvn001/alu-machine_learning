ada92c68a45f1f930a80ce429e369d4a5d14bcab
KvnKl007 <klakkent99@gmail.com>
2024-09-10 22:09:07 +0200

    doc

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,7 +3,21 @@
 
 def matrix_shape(matrix):
     """
-    This function takes a matrix as input and returns its shape.
+    Returns the shape of a matrix (a list of lists) as a list of integers.
+
+    The shape of a matrix is a list of integers, where each integer represents
+    the number of elements in each dimension of the matrix. For example, a 2x3
+    matrix would have a shape of [2, 3].
+
+    Args:
+        matrix (list): A list of lists, representing a matrix.
+
+    Returns:
+        list: A list of integers, representing the shape of the matrix.
+
+    Example:
+        >>> matrix_shape([[1, 2, 3], [4, 5, 6]])
+        [2, 3]
     """
     shape = []
     while isinstance(matrix, list):
