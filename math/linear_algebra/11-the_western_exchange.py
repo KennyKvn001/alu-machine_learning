952b4c845814bfcd3afb2259d37e5d6a4ec1caea
KvnKl007 <klakkent99@gmail.com>
2024-09-12 18:45:41 +0200

    The western exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module `10-ill_use_my_scale`
+This module contains a function that cthat transposes matrix
+"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """
+    This function that transpose a matrix a numpy.ndarray
+    """
+    return matrix.T
