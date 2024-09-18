39d8314dd165ac5088bfe9a4c65956da61fee1bc
KennyKvn001 <k.mugisha@alustudent.com>
2024-09-19 00:14:23 +0200

    1-minor

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -13,8 +13,9 @@ def minor(matrix):
     deleting the row and column of that element.
 
     Args:
-    matrix (list of lists): The input matrix whose minor matrix is to be calculated.
-                            Each inner list represents a row of the matrix.
+    matrix (list of lists): The input matrix whose minor matrix is to be
+    calculated.
+    Each inner list represents a row of the matrix.
 
     Returns:
     list of lists: The minor matrix of the input matrix.
@@ -25,8 +26,7 @@ def minor(matrix):
 
     """
 
-    if not isinstance(matrix, list) or not all(
-            isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     n = len(matrix)
@@ -44,7 +44,7 @@ def minor(matrix):
             return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]
         det = 0
         for j in range(len(mat)):
-            submatrix = [row[:j] + row[j + 1:] for row in mat[1:]]
+            submatrix = [row[:j] + row[j + 1 :] for row in mat[1:]]
             det += ((-1) ** j) * mat[0][j] * determinant(submatrix)
         return det
 
@@ -53,7 +53,7 @@ def minor(matrix):
         minor_row = []
         for j in range(n):
             submatrix = [
-                row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])
+                row[:j] + row[j + 1 :] for row in (matrix[:i] + matrix[i + 1 :])
             ]
             minor_row.append(determinant(submatrix))
         minor_matrix.append(minor_row)
