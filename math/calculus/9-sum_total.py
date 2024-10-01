3e6524ff88ef2489379bcf1e22800f6515700855
KennyKvn001 <k.mugisha@gmail.com>
2024-10-02 00:16:24 +0200

    9-sum_total

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 """
-This script contains a function to calculate the sum of the squares of the first n natural numbers.
+This script contains a function to calculate the sum
+of the squares of the first n natural numbers.
 """
 
 
 def summation_i_squared(n):
     """
-    This function calculates the sum of the squares of the first n natural numbers.
+    This function calculates the sum of the squares
+    of the first n natural numbers.
 
     Parameters:
     n (int): The number of natural numbers to sum.
 
     Returns:
-    int: The sum of the squares of the first n natural numbers. Returns None if n is not an integer.
+    int: The sum of the squares of the first n natural numbers.
+    Returns None if n is not an integer.
     """
     if not isinstance(n, int) or n <= 0:
         return None
