02562b8b61d99bb77d3bb6114a2dad85299f4d9b
KennyKvn001 <k.mugisha@gmail.com>
2024-10-02 21:42:45 +0200

    17-integrate

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -7,25 +7,30 @@ of a polynomial.
 
 def poly_integral(poly, C=0):
     """
-    This function computes the integral of a polynomial represented by a list of coefficients.
+    This function computes the integral of a polynomial
+    represented by a list of coefficients.
 
     Parameters:
-    - poly: A list of coefficients, where the index of each element represents the power of x.
-    - C: An optional integer representing the constant of integration (default is 0).
+    - poly: A list of coefficients, where the index of each
+    element represents the power of x.
+    - C: An optional integer representing the constant
+    of integration (default is 0).
 
     Returns:
-    - A new list of coefficients representing the integral of the polynomial.
-      If the result contains a term with a whole number, it should be returned as an integer.
-      If the input poly or C are not valid, the function returns None.
+    - A new list of coefficients representing the
+    integral of the polynomial.
+      If the result contains a term with a whole number,
+      it should be returned as an integer.
+      If the input poly or C are not valid,
+      the function returns None.
     """
-
-    integral = [C]
 
     if not isinstance(poly, list) or not isinstance(C, (int, float)):
         return None
     if not all(isinstance(coef, (int, float)) for coef in poly):
         return None
 
+    integral = [C]
     # Iterate through the coefficients of the input polynomial.
     for i, coef in enumerate(poly):
         # Calculate the new coefficient after integrating (coef / (i + 1))
