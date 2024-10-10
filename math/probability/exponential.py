484d77b10ee45a7702f8286c20df050b4d68f125
KennyKvn001 <k.mugisha@alustudent.com>
2024-10-10 11:50:52 +0200

    Exponentials

---
+++ math/probability/exponential.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" Exponential class  """
+
+
+class Exponential:
+    """
+    class that represents exponential distribution
+
+    class constructor:
+        def __init__(self, data=None, lambtha=1.)
+
+    instance attributes:
+        lambtha [float]: the expected number of occurances in a given time
+
+    instance methods:
+        def pdf(self, x): calculates PDF for given time period
+        def cdf(self, x): calculates CDF for given time period
+    """
+
+    def __init__(self, data=None, lambtha=1.0):
+        """
+        class constructor
+
+        parameters:
+            data [list]: data to be used to estimate the distibution
+            lambtha [float]: the expected number of occurances on a given time
+
+        Sets the instance attribute lambtha as a float
+        If data is not given:
+            Use the given lambtha or
+            raise ValueError if lambtha is not positive value
+        If data is given:
+            Calculate the lambtha of data
+            Raise TypeError if data is not a list
+            Raise ValueError if data does not contain at least two data points
+        """
+        if data is None:
+            if lambtha < 1:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                lambtha = float(len(data) / sum(data))
+                self.lambtha = lambtha
